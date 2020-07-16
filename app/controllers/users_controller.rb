d684a6677be043e83d767262a7f89505d3dc0246
Bach <hoangbachilt@gmail.com>
2020-07-16 14:15:26 +0700

    chapter 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t ".new.signup_success"
-      redirect_to @user
+      @user.send_mail_activate
+      flash[:success] = t ".mail_notice"
+      redirect_to root_path
     else
       flash[:danger] = t ".new.signup_fail"
       render :new
