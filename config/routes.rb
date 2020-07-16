8028041a56532f954e70f0725830ee80e3c88dba
Bach <hoangbachilt@gmail.com>
2020-07-16 14:15:26 +0700

    chapter 12

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
-    resources :users, except: :new
-    resources :account_activations, only: :edit
+    resources :users, except: %i(new)
+    resources :account_activations, only: %i(edit)
+    resources :password_resets, except: %i(index show destroy)
   end
 end
