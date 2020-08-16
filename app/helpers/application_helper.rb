3d01a24ba2b2e887fcd7d2ff0e54b895bb8bb3ca
Bach <hoangbachilt@gmail.com>
2020-08-16 16:04:23 +0700

    test rebase

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,6 @@ module ApplicationHelper
     base_title =  t "layouts.application.title_app"
     page_title.empty? ? base_title : [page_title, base_title].join(" | ")
   end
+
+test rebase 1-1-2
 end
